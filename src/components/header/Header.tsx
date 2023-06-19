26ce8112d287ff6ed8aa922e2907b4ae28651c3a
DESKTOP-94VSVD4\CuanHoangDe <quannguyen2724@gmail.com>
2023-06-20 01:06:11 +0700

    remove excessive code

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { ConnectWallet } from '../connectWallet';
 import { Wallet } from '../Wallet';
 
 declare global {
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export * from './header'
-export * from './connectWallet'
 export * from './Wallet'
