d49766449fba4afa6e1d727983f360774f008456
DESKTOP-94VSVD4\CuanHoangDe <quannguyen2724@gmail.com>
2023-06-20 01:57:25 +0700

    more detail for routing example

--- src/pages/items/index.ts
+++ src/pages/items/index.ts
@@ -3,9 +3,10 @@ import { compose } from 'ramda'
 import { withApi } from './with-api'
 import { withStyle } from './with-style'
 import { withCssModule } from './with-css-module'
-
-export const Items = compose(
+ const Items = compose(
   withCssModule,
   withStyle,
   withApi
 )(ItemsBase)
+
+export default Items;
